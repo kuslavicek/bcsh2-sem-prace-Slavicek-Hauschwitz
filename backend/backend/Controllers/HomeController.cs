bd1e27e19446b85e918a50653838fbf917bea53f
Kuslavicek <38736526+Kuslavicek@users.noreply.github.com>
2023-10-21 20:51:42 +0200

    Začátek frontendu a backendu

---
+++ backend/backend/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Diagnostics;
+
+namespace backend.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Test() {
+            DataTable table = DatabaseAccess.GetDataFromOracle();
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
