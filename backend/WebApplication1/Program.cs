d1bca38f2f2b3d5ffeb8c7e5e020f98557fc8db5
Kuslavicek <38736526+Kuslavicek@users.noreply.github.com>
2023-12-17 20:09:47 +0100

    Cors opraveno

--- backend/WebApplication1/Program.cs
+++ backend/WebApplication1/Program.cs
@@ -1,15 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
-var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: MyAllowSpecificOrigins,
-                      policy =>
-                      {
-                          policy.WithOrigins("http://localhost:3000");
-                      });
-});
+
 var app = builder.Build();
 
 
@@ -20,10 +12,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-app.UseCors(MyAllowSpecificOrigins);
-app.UseHttpsRedirection();
-app.UseStaticFiles();
 
+app.UseStaticFiles();
+app.UseCors(builder =>
+        builder
+        .WithOrigins("http://localhost")
+        .AllowAnyMethod()
+        .AllowAnyHeader());
 app.UseRouting();
 
 app.UseAuthorization();
