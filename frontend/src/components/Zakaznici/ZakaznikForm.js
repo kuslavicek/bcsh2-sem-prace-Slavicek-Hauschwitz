04514381ca814da4f3755ee95df197ffa6c60e80
Kuslavicek <38736526+Kuslavicek@users.noreply.github.com>
2023-12-18 20:50:57 +0100

    Vytvoření + úprava zákazníka

--- frontend/src/components/Zakaznici/ZakaznikForm.js
+++ frontend/src/components/Zakaznici/ZakaznikForm.js
@@ -1,28 +1,94 @@
-function ZakaznikForm(i=0){
-    
-    const url = "https://localhost:7043/api/zakaznik?jmeno=Pepa&ulice=Vrchní&mesto=Pardubice&cislo_popisne=532&psc=53509&telefon=732732512&email=pepa@gmail.com&stat=ČR"
+import React, { useState, useEffect } from 'react';
 
-    if(i=0){
-        return(
+function ZakaznikForm({ id }) {
+    const [formData, setFormData] = useState({
+        jmeno: '',
+        ulice: '',
+        mesto: '',
+        cislo_popisne: '',
+        psc: '',
+        telefon: '',
+        email: '',
+        stat: ''
+    });
+
+    const fetchZakaznikData = async () => {
+        if (id !== 0) {
+            try {
+                const response = await fetch(`https://localhost:7043/api/zakaznik?id=${id}`);
+                if (response.ok) {
+                    const data = await response.json();
+                    setFormData(data); // Nastavení dat získaných z API
+                } else {
+                    throw new Error('Nepodařilo se načíst data z API');
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    };
+
+    useEffect(() => {
+        fetchZakaznikData();
+    }, [id]);
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const requestOptions = {
+                method: id === 0 ? 'POST' : 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            };
+
+            const response = await fetch(
+                id === 0
+                    ? 'https://localhost:7043/api/zakaznik'
+                    : `https://localhost:7043/api/zakaznik?id=${id}`,
+                requestOptions
+            );
+
+            if (response.ok) {
+                // Zde můžeš zpracovat odpověď, pokud je potřeba
+            } else {
+                throw new Error('Nepodařilo se odeslat data');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="jmeno" value={formData.jmeno} onChange={handleInputChange} />
+                <input type="text" name="ulice" value={formData.ulice} onChange={handleInputChange} />
+                <input type="text" name="mesto" value={formData.mesto} onChange={handleInputChange} />
+                <input
+                    type="text"
+                    name="cislo_popisne"
+                    value={formData.cislo_popisne}
+                    onChange={handleInputChange}
+                />
+                <input type="text" name="psc" value={formData.psc} onChange={handleInputChange} />
+                <input type="text" name="telefon" value={formData.telefon} onChange={handleInputChange} />
+                <input type="text" name="email" value={formData.email} onChange={handleInputChange} />
+                <input type="text" name="stat" value={formData.stat} onChange={handleInputChange} />
 
-
-
+                <button type="submit">{id === 0 ? 'Odeslat' : 'Upravit'}</button>
             </form>
-
         </div>
-    )
-    }else{
-        return(
-            <div>
-                <form>
-
-
-                
-                </form>
-            </div>
-        )
-    }
+    );
 }
+
 export default ZakaznikForm;
