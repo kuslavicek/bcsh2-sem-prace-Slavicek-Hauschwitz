025536500fd295284a12f34781b1d13531406a2b
Kuslavicek <38736526+Kuslavicek@users.noreply.github.com>
2024-10-31 19:09:45 +0100

    desktop aplikace

---
+++ App/App/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            listView1 = new ListView();
+            listView2 = new ListView();
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            tabPage2.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(12, 12);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(1296, 785);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(button3);
+            tabPage1.Controls.Add(button2);
+            tabPage1.Controls.Add(button1);
+            tabPage1.Controls.Add(listView2);
+            tabPage1.Controls.Add(listView1);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(1288, 757);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Controls.Add(button5);
+            tabPage2.Controls.Add(button4);
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(1288, 757);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(18, 55);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(878, 304);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // listView2
+            // 
+            listView2.Location = new Point(18, 386);
+            listView2.Name = "listView2";
+            listView2.Size = new Size(875, 294);
+            listView2.TabIndex = 1;
+            listView2.UseCompatibleStateImageBehavior = false;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(1059, 91);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 2;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(1059, 220);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 3;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(1059, 358);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 23);
+            button3.TabIndex = 4;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(506, 103);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 23);
+            button4.TabIndex = 0;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(506, 335);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 23);
+            button5.TabIndex = 1;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1320, 809);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage2.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private ListView listView2;
+        private ListView listView1;
+        private Button button5;
+        private Button button4;
+    }
+}
---
+++ App/App/Models.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class Models
+    {
+public class Adresa
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(32)]
+        public string Ulice { get; set; }
+        [Required, MaxLength(32)]
+        public string Mesto { get; set; }
+        [Required]
+        public int Psc { get; set; }
+        [Required]
+        public int CisloPopisne { get; set; }
+        [Required, MaxLength(32)]
+        public string Stat { get; set; }
+    }
+
+    public class Akce
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int PocetOsob { get; set; }
+        [Required]
+        public DateTime Datum { get; set; }
+        [ForeignKey("TypAkce")]
+        public int IdTypAkce { get; set; }
+        [ForeignKey("Objednavka")]
+        public int IdObjednavka { get; set; }
+    }
+
+    public class Cider
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(255)]
+        public string OdrudaJablek { get; set; }
+    }
+
+    public class Faktura
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public byte[] FakturaData { get; set; } // For BLOB
+        [Required, MaxLength(255)]
+        public string NazevSouboru { get; set; }
+        [Required]
+        public DateTime DatumVlozeni { get; set; }
+    }
+
+    public class Logs
+    {
+        [Key]
+        public DateTime Datum { get; set; }
+        [MaxLength(2000)]
+        public string Text { get; set; }
+    }
+
+    public class ObjednaneZbozi
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Mnozstvi { get; set; }
+        [ForeignKey("Objednavka")]
+        public int IdObjednavka { get; set; }
+        [ForeignKey("Zbozi")]
+        public int IdZbozi { get; set; }
+    }
+
+    public class Objednavka
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime DatumZalozeni { get; set; }
+        [Required]
+        public decimal Cena { get; set; }
+        [ForeignKey("Zakaznik")]
+        public int IdZakaznik { get; set; }
+        [ForeignKey("Faktura")]
+        public int IdFaktura { get; set; }
+    }
+
+    public class Pivo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Stupnovitost { get; set; }
+    }
+
+    public class PracovniPozice
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(32)]
+        public string Nazev { get; set; }
+        [Required]
+        public decimal Plat { get; set; }
+    }
+
+    public class Provozovna
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(32)]
+        public string Nazev { get; set; }
+        [Required]
+        public int PocetZamestnancu { get; set; }
+        [ForeignKey("Adresa")]
+        public int IdAdresa { get; set; }
+    }
+
+    public class Sklad
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public decimal UzitnaPlocha { get; set; }
+        [ForeignKey("Adresa")]
+        public int IdAdresa { get; set; }
+        [Required, MaxLength(255)]
+        public string Nazev { get; set; }
+    }
+
+    public class Surovina
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(32)]
+        public string Nazev { get; set; }
+        [Required]
+        public int Mnozstvi { get; set; }
+        [ForeignKey("Sklad")]
+        public int IdSklad { get; set; }
+    }
+
+    public class TypAkce
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(32)]
+        public string Nazev { get; set; }
+    }
+
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(20)]
+        public string Jmeno { get; set; }
+        [Required, MaxLength(50)]
+        public string Heslo { get; set; }
+        [Required, MaxLength(50)]
+        public string Role { get; set; }
+        [Required]
+        public int NezobrazovatOs { get; set; } = 0;
+        [ForeignKey("Zamestnanec")]
+        public int IdZamestnanec { get; set; }
+    }
+
+    public class Zakaznik
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(32)]
+        public string Jmeno { get; set; }
+        public int? Telefon { get; set; }
+        [MaxLength(32)]
+        public string Email { get; set; }
+        [ForeignKey("Adresa")]
+        public int IdAdresa { get; set; }
+    }
+
+    public class Zamestnanec
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(32)]
+        public string Jmeno { get; set; }
+        [Required, MaxLength(32)]
+        public string Prijmeni { get; set; }
+        [MaxLength(32)]
+        public string Email { get; set; }
+        public int? Telefon { get; set; }
+        [ForeignKey("Provozovna")]
+        public int IdProvozovna { get; set; }
+        [ForeignKey("PracovniPozice")]
+        public int IdPracovniPozice { get; set; }
+        [ForeignKey("Zamestnanec")]
+        public int? IdNadrizeny { get; set; }
+    }
+
+    public class Zbozi
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(255)]
+        public string Nazev { get; set; }
+        [Required]
+        public decimal ObsahAlkoholu { get; set; }
+        [Required]
+        public decimal Cena { get; set; }
+        [ForeignKey("Sklad")]
+        public int IdSklad { get; set; }
+        [Required, MaxLength(1)]
+        public char Typ { get; set; }
+    }
+
+}
+}
