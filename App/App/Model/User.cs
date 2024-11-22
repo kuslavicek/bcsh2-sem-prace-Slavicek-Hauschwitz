8dcaa8b9d16700888024bf75176993e6cc83c4b0
matejhaus <matej.hauschwitz@seznam.cz>
2024-11-22 01:26:07 +0100

    User příprava

---
+++ App/App/Model/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Jmeno { get; set; }
+        public string Heslo { get; set; }
+        public string Role { get; set; }
+        public int boolean { get; set; }
+        public int IdZamestnanec { get; set; }
+    }
+}
