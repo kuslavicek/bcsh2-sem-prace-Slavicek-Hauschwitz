1792cdbf02220118053d9774fa9d47af9328f406
matejhaus <matej.hauschwitz@seznam.cz>
2024-11-27 17:53:17 +0100

    změna api

--- App/App/Extensions/StateLoader.cs
+++ App/App/Extensions/StateLoader.cs
@@ -19,15 +19,20 @@ namespace App.Extensions
         {
             using (var client = new HttpClient())
             {
-                string apiUrl = "https://restcountries.com/v3.1/all";
+                string apiUrl = "https://countriesnow.space/api/v0.1/countries/";
                 var response = await client.GetStringAsync(apiUrl);
 
-                var countries = JsonConvert.DeserializeObject<List<Country>>(response);
+                var apiResponse = JsonConvert.DeserializeObject<ApiResponse>(response);
 
-                var states = countries.Select(c => new State
+                if (apiResponse == null || apiResponse.Data == null)
                 {
-                    Name = c.name.common,
-                    Code = c.cca2
+                    throw new Exception("Invalid response or no data returned from API.");
+                }
+
+                var states = apiResponse.Data.Select(c => new State
+                {
+                    Name = c.Country,
+                    Code = c.Iso2
                 }).ToList();
 
                 return states;
@@ -35,14 +40,29 @@ namespace App.Extensions
         }
     }
 
-    public class Country
+    public class ApiResponse
     {
-        public Name name { get; set; }
-        public string cca2 { get; set; }
-    }
+        [JsonProperty("error")]
+        public bool Error { get; set; }
 
-    public class Name
+        [JsonProperty("msg")]
+        public string Message { get; set; }
+
+        [JsonProperty("data")]
+        public List<Countri> Data { get; set; }
+    }
+    public class Countri
     {
-        public string common { get; set; }
+        [JsonProperty("iso2")]
+        public string Iso2 { get; set; }
+
+        [JsonProperty("iso3")]
+        public string Iso3 { get; set; }
+
+        [JsonProperty("country")]
+        public string Country { get; set; }
+
+        [JsonProperty("cities")]
+        public List<string> Cities { get; set; }
     }
 }
